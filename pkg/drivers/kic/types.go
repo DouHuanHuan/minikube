6c43f4aba54845e913e3350a145dab61567c8d75
douzengrui <douzengrui@gmail.com>
2025-02-07 14:50:32 +0800

    修改镜像地址

--- pkg/drivers/kic/types.go
+++ pkg/drivers/kic/types.go
@@ -29,7 +29,7 @@ const (
 	// SHA of the kic base image
 	baseImageSHA = "7b3f6168a578563fb342f21f0c926652b91ba060931e8fbc6c6ade3ac1d26ed9"
 	// The name of the GCR kicbase repository
-	gcrRepo = "gcr.io/k8s-minikube/kicbase-builds"
+	gcrRepo = "swr.cn-north-4.myhuaweicloud.com/ddn-k8s/gcr.io/k8s-minikube/kicbase-builds"
 	// The name of the Dockerhub kicbase repository
 	dockerhubRepo = "docker.io/kicbase/build"
 )
